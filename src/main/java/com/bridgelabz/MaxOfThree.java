27b30e70f19474a10ee55bb04a269361cd41cdce
KumarB95 <kumarbalingal@gmail.com>
2021-10-04 10:43:39 +0530

    Refactored : Code is refactored to generic to find Maximum

--- src/main/java/com/bridgelabz/MaxOfThree.java
+++ src/main/java/com/bridgelabz/MaxOfThree.java
@@ -8,8 +8,8 @@ public class MaxOfThree {
         Float[] floatArray={12.5f,25.35f,50.25f};
         String[] stringArray={"Apple","Peach","Banana"};
 
-        testCases.findMaxInThree(intArray);
-        testCases.findMaxFloat(floatArray);
-        testCases.findMaxString(stringArray);
+        System.out.println("Maximum of int [15,12,13] is : "+testCases.findMax(intArray));
+        System.out.println("Maximum of Float [12.5f,25.35f,50.25f] is : "+testCases.findMax(floatArray));
+        System.out.println("Maximum of String [\"Apple\",\"Peach\",\"Banana\"] is : "+testCases.findMax(stringArray));
     }
 }
--- src/main/java/testOfMax/testCases.java
+++ src/main/java/testOfMax/testCases.java
@@ -1,38 +1,21 @@
 package testOfMax;
-import org.junit.*;
-import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 
-public class testCases {
-    public static void findMaxInThree(Integer[] intArray) {
-        int max=0;
-        for (int i=0;i< intArray.length;i++){
-            if (intArray[i]>max){
-                max=intArray[i];
-            }
-        }
-        System.out.println("Max number is : "+max);
-    }
+public class testCases <E extends Comparable<E>>{
+    private E[] inputArray;
 
-    public void findMaxFloat(Float[] floatArray) {
-        float max=0;
-        for (int i=0;i< floatArray.length;i++){
-            if (floatArray[i]>max){
-                max=floatArray[i];
-            }
-        }
-        System.out.println("Max number is : "+max);
+    public void Maximum(E[] inputArray) {
+        this.inputArray=inputArray;
     }
-
-    public void findMaxString(String[] stringArray) {
-        String max=stringArray[0];
-        for (int i=1;i<stringArray.length;i++){
-            if (stringArray[i].compareTo(max)>0){
-                max=stringArray[i];
+    public static <E extends Comparable> E findMax(E[] inputArray){
+        E max=inputArray[0];
+        for (int i=1;i<inputArray.length;i++){
+            if (inputArray[i].compareTo(max)>0){
+                max=inputArray[i];
             }
         }
-        System.out.println("Max String is : "+max);
+        return max;
     }
 }
