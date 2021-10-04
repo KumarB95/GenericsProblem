010b2761db4b09668a861c1d2a9ebfde7698dbde
KumarB95 <kumarbalingal@gmail.com>
2021-10-04 11:02:13 +0530

    Refactored : Added Method to print maximum using Generic

--- src/main/java/com/bridgelabz/MaxOfThree.java
+++ src/main/java/com/bridgelabz/MaxOfThree.java
@@ -12,9 +12,9 @@ public class MaxOfThree {
         String[] stringArray={"Apple","Peach","Banana"};
         Integer[] intArray1={10,12,25,58,54,48};
 
-        System.out.println("Maximum of int [15,12,13] is : "+testCases.findMax(intArray));
-        System.out.println("Maximum of Float [12.5f,25.35f,50.25f] is : "+testCases.findMax(floatArray));
-        System.out.println("Maximum of String [\"Apple\",\"Peach\",\"Banana\"] is : "+testCases.findMax(stringArray));
-        System.out.println("Maximum of More than three numbers {10,12,25,58,54,48} is : "+testCases.findMax(intArray1));
+        testCases.findMax(intArray);
+        testCases.findMax(floatArray);
+        testCases.findMax(stringArray);
+        testCases.findMax(intArray1);
     }
 }
--- src/main/java/testOfMax/testCases.java
+++ src/main/java/testOfMax/testCases.java
@@ -13,13 +13,16 @@ public class testCases <E extends Comparable<E>>{
         this.inputArray=inputArray;
     }
 
-    public static <E extends Comparable> E findMax(E[] inputArray){
+    public static <E extends Comparable> void findMax(E[] inputArray){
         E max=inputArray[0];
         for (int i=1;i<inputArray.length;i++){
             if (inputArray[i].compareTo(max)>0){
                 max=inputArray[i];
             }
         }
-        return max;
+        printMax(max);
+    }
+    private static<T> void printMax(T max) {
+        System.out.println("Maximum is : "+max);
     }
 }
